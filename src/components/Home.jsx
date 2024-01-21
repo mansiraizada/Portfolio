61fbb8ba1fd251a524da288d169b343d79f21f3a
mansiraizada <mansi23raizada@gmail.com>
2024-01-21 20:08:53 +0530

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,19 +7,22 @@ const Home = () => {
 
     useGSAP(() => {
         tl.from('.text h1', {
+            delay: 1,
             y: 500,
-            duration: 1,
+            duration: 0.6,
             opacity: 0,
-            stagger: 0.5,
+            stagger: 0.4,
         })
     })
 
-  return (
-      <div className='text overflow-hidden p-1 w-full h-[100vh] flex flex-col justify-center items-center text-center font-bold'>
+    return (
+      <div className='w-full h-[100vh] flex items-end'>
+      <div className='text overflow-hidden p-1 w-full h-[92vh] flex flex-col justify-center items-center text-center font-bold'>
           <h1 className='text-5xl md:text-6xl'>Hi there! </h1> 
           <h1 className='text-5xl md:text-6xl mt-2'>I'm </h1>
           <h1 className='text-5xl md:text-8xl mt-4'>Mansi Raizada</h1>
           <h1 className='mt-[3vh] p-5 text-sm md:text-2xl'>I build pixel-perfect, accessible products for the web and beyond.</h1>
+      </div>
       </div>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,27 @@
 import React, {useState} from 'react'
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { CgMenuGridO } from "react-icons/cg";
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Nav = () => {
   const [mobileNav, setMobileNav] = useState(false);
 
+  useGSAP(() => {
+    gsap.from('.nav', {
+      y:-100,
+      opacity: 0,
+      duration: 1,
+    })
+  })
   const navHandler = () => {
     setMobileNav(!mobileNav)
   }
 
   return (
-    <div className='w-full h-[8vh] fixed bg-[#949494] z-[9998]'>
+    <div className='nav w-full h-[8vh] fixed bg-[#949494] z-[9998]'>
       <div className='z-[9999] w-full h-full flex'>
-          <div className='hidden w-[50vw] md:flex items-center py-4 px-8 gap-8 font-semibold'>
+          <div className='hidden w-[50vw] md:flex items-center py-4 px-10 gap-8 font-semibold'>
           <h3 className='hover:cursor-pointer hover:scale-[1.1] duration-100 ease-in'>Home</h3>
           <h3 className='hover:cursor-pointer hover:scale-[1.1] duration-100 ease-in'>About</h3>
           <h3 className='hover:cursor-pointer hover:scale-[1.1] duration-100 ease-in'>Skills</h3>
@@ -23,7 +32,7 @@ const Nav = () => {
         <div className='w-[50vw] flex md:hidden items-center px-4'>
           <CgMenuGridO className='text-3xl hover:cursor-pointer' onClick={navHandler}/>
         </div>
-          <div className='w-[50vw] flex justify-end items-center gap-3 md:gap-5 px-4 md:px-0 md:mr-8'>
+          <div className='w-[50vw] flex justify-end items-center gap-3 md:gap-5 px-4 md:px-2 md:mr-8'>
             <FaLinkedin className='text-2xl hover:cursor-pointer'/>
             <FaGithub className='text-2xl hover:cursor-pointer'/>
           </div>  

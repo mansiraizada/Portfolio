b49837a9a272a634082be8e468e075277ec58cd1
mansiraizada <mansi23raizada@gmail.com>
2024-01-23 21:29:25 +0530

    Skills completed

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <Home/>
       <About/>
       <Skills/>
-      <Project/>
+      {/* <Project/> */}
     </>
   )
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div className='w-full h-[100vh] bg-red-200'>Project</div>
+    <div className='w-full h-[100vh] bg-[#111827] text-white'>Project</div>
   )
 }
 

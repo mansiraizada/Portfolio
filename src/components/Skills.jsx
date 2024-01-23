932e8887588f71de4b2a7d5fbf6dbb2a105bea42
mansiraizada <mansi23raizada@gmail.com>
2024-01-23 12:37:03 +0530

    Updated Skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,20 +4,22 @@ const techSkills = ['JavaScript', 'CSS', 'HTML', 'REDUX', 'REACT', 'JEST', 'TAIL
 
 const Skills = () => {
   return (
-    <div className='w-full h-[100vh] bg-gray-500 p-4 py-6 md:p-12'>
-      <h1 className='font-bold text-4xl uppercase'>Skills</h1>
+    <div className='w-full h-[100vh] bg-gray-500 py-6 md:p-12'>
+      <h1 className='font-bold text-4xl uppercase px-4'>Skills</h1>
       <div className='w-full h-[95%] pt-2 md:p-10 md:flex'>
         <div className='w-full h-fit md:h-[100%]'>
-          <h1 className='text-xl md:text-2xl font-bold uppercase pb-6'>Development</h1>
-          <div className='w-full h-[fit] flex flex-wrap justify-center gap-2'>
+          <h1 className='text-xl md:text-2xl font-bold uppercase px-4 pb-4'>Development</h1>
+          <div className='py-2 w-full md:w-[80%] h-[fit] md:h-[90%] flex flex-wrap justify-center gap-2 md:grid md:grid-cols-3 md:gap-[0] md:p-8 md:py-0'>
             {techSkills.map((skill, i) =>
-            <div key={i} className='w-[12vh] h-[12vh] text-white rounded-full text-center flex items-center justify-center font-semibold text-xs uppercase bg-gray-700'>
+            <div key={i} className='w-[12vh] md:w-[9vw] h-[12vh] md:h-[9vw] text-white rounded-full text-center flex items-center justify-center font-semibold text-xs md:text-sm uppercase bg-gray-700'>
               {skill}
             </div>
             )}
           </div> 
         </div>
-        <div className='bg-yellow-300 w-full md:h-[100%]'></div>
+        <div className='bg-yellow-300 w-full md:h-[100%]'>
+           <h1 className='text-xl md:text-2xl font-bold uppercase px-4 pb-4'>Tools & Knowledge</h1>
+        </div>
       </div>
     </div>
   )
